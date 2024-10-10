3f731d5d5413f51a61c331ee6fef76fd27477596
Ian G. <iangaunt999@gmail.com>
2024-10-09 19:59:32 -0700

    Edited some text in NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar(): JSX.Element {
     return (
         <div className="navbar">
-            <p className="idx">IDX Test App</p>
+            <p className="idx">./idx-test</p>
             <Link href="/home/page">Home</Link>
             <Link href="/account/page">Account</Link>
             <Link href="/help/page">Help</Link>
