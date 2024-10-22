b53fc0d5c6c9a66efd8298a9b665778a09671035
Ian G. <iangaunt999@gmail.com>
2024-10-22 16:38:45 -0700

    Added preliminary media scaling

--- src/pages/blog/articles/2024/reading.tsx
+++ src/pages/blog/articles/2024/reading.tsx
@@ -1,5 +1,4 @@
 import BlogBack from "@/components/BlogBack";
-import Codeblock from "@/components/Codeblock";
 import Navbar from "@/components/Navbar";
 import Template from "@/pages/template";
 
@@ -14,7 +13,7 @@ export default function Page() {
                 <Template >
                     <div className={styles.container}>
                         <BlogBack></BlogBack>
-                        <h1>→ <span>[10.21.24]</span> my reading list</h1>
+                        <h1>→ <span>[10.22.24]</span> my reading list</h1>
 
                         <p>
                             I like reading books. Unfortunately, I have had no time to read them, so I'll use this
@@ -23,9 +22,21 @@ export default function Page() {
                         </p>
                         
                         <h2>Computer Science</h2>
-                        <p>
-                            The Algorithm Design Manual - Steven S. Skierna
-                        </p>
+                        <ul>
+                            <li>The Algorithm Design Manual - Steven S. Skierna </li>
+                            <li>Structure and Interpretation of Computer Programs - Harold Abelson, Gerald Jay Sussman </li>
+                            <li>The Object Oriented Thought Process - Matt Weisfield </li>
+                            <li>Introduction to Algorithms, 4th ed. - Thomas H. Cormen </li>
+                            <li>Introduction to Compilers and Language Design - Douglas Thain</li>
+                            <li> Computer Networking: A Top-Down Approach - Kurose & Ross</li>
+                        </ul>
+
+                        <h2>Music Production</h2>
+                        <ul>
+                            <li>The Art of Mixing - David Gibson</li>
+                            <li>Mastering Audio: the Art and the Science - Robert A. Katz </li>
+                            <li>Honestly, I'm just trying to get the Ableton docs ...</li>
+                        </ul>
                         
                         <footer>Happy coding! :) <a href="#top">(↑ back to top)</a></footer>
                     </div>  
--- src/pages/blog/page.tsx
+++ src/pages/blog/page.tsx
@@ -6,6 +6,11 @@ import Article from "./Article";
 
 import { Icon } from '@iconify/react';
 
+const y1: string = "rgb(255 209 102)";
+const y2: string = "rgb(172 210 124)";
+const g1: string = "rgb(89 213 143)";
+const g2: string = "rgb(6 214 160)";
+
 export default function Blog() {
     return (
         <>
@@ -17,36 +22,44 @@ export default function Blog() {
                             <Article 
                                 title="chip8 emulation"
                                 body="A quick post about how I built a chip8 emulator from scratch with C++, SDL2, and Visual Studio Code."
+                                
                                 date="[10.21.24]"
                                 url="/blog/articles/2024/chip8"
+
                                 icon={<Icon icon="hugeicons:chip" />}
-                                iconColor={"rgb(255 209 102)"}
+                                iconColor={y1}
                             ></Article>
                             <Article 
                                 title="how i use obsidian"
                                 body="I take a lot of notes in college. Obsidian is my go-to notes app and I'll let you know why."
+
                                 date="[10.11.24]"
                                 url="https://google.com/"
+
                                 icon={<Icon icon="simple-icons:obsidian" />}
-                                iconColor={"rgb(172 210 124)"}
+                                iconColor={y2}
                             ></Article>
                         </div>
                         <div className={styles.row}>
                             <Article 
                                 title="programming"
                                 body="I like programming language design. Quite a lot of them, however, have some weird design quirks."
+
                                 date="[10.11.24]"
                                 url="https://google.com/"
+
                                 icon={<Icon icon="basil:folder-solid" />}
-                                iconColor={"rgb(89 213 148)"}
+                                iconColor={g1}
                             ></Article>
                             <Article 
                                 title="reading list"
                                 body="Here's a few textbooks I have my eyes on, or ones that I'm currently reading and working through."
-                                date="[10.11.24]"
+
+                                date="[10.22.24]"
                                 url="/blog/articles/2024/reading"
+
                                 icon={<Icon icon="basil:book-open-solid" />}
-                                iconColor={"rgb(6 214 160)"}
+                                iconColor={g2}
                             ></Article>
                         </div>
                     </div>
